2a6cb37bdc5779ca3d9e88624577b4d14c8812db
Chantelsky <chantel.sky@outlook.com>
2020-04-26 20:54:12 +1000

    browser tabs with onMouseOut and onMouseMove

---
+++ app3-browsertabs/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Tab({ children }) {
+  const [hightlightStyle, setHighlightStyle] = useState({
+    left: 0,
+    opacity: 0
+  });
+
+  function moveHighlight(e) {
+    //update highlight style to move the highlight
+    setHighlightStyle({
+      left: -e.nativeEvent.layerX - 150
+    });
+  }
+
+  function hideHighlight(e) {
+    setHighlightStyle({
+      opacity: 0,
+      left: -e.nativeEvent.layerX - 150
+    });
+  }
+  return (
+    <div className='tab' onMouseOut={hideHighlight} onMouseMove={moveHighlight}>
+      <div className='highlight' style={hightlightStyle} />
+      {children}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='browser'>
+        <div className='tabs'>
+          <Tab>
+            {' '}
+            <a>Home</a>
+          </Tab>
+          <Tab>
+            {' '}
+            <a>About</a>
+          </Tab>
+          <Tab>
+            {' '}
+            <a>Features</a>
+          </Tab>
+        </div>
+        <div className='viewport'>Pages Go Here</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
