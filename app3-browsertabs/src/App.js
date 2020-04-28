5e3c747ee614e377ed4ec82be593224030349848
Chantelsky <chantel.sky@outlook.com>
2020-04-28 21:11:20 +1000

    react router

--- app3-browsertabs/src/App.js
+++ app3-browsertabs/src/App.js
@@ -1,47 +1,56 @@
-import React from 'react';
+import React from "react";
 import {
-  BrowserRouter as Router, Switch, Route, NavLink
-} from 'react-router-dom'
-import Tab from './components/Tab'
-import Home from './pages/Home'
-import Features from './pages/Features'
-import About from './pages/About'
-import './App.css';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import Tab from "./components/Tab";
+import Home from "./pages/Home";
+import Features from "./pages/Features";
+import About from "./pages/About";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-    <div className='app'>
-      <div className='browser'>
-        <div className='tabs'>
-          <Tab>
-            {' '}
-            <NavLink to="/" exact={true} activeClassName="is-active">Home</NavLink>
-          </Tab>
-          <Tab>
-            {' '}
-            <NavLink to="/about" activeClassName="is-active">About</NavLink>
-          </Tab>
-          <Tab>
-            {' '}
-            <NavLink to="/features" activeClassName="is-active">Features</NavLink>
-          </Tab>
-        </div>
-        <div className='viewport'>
-          <Switch>
-        <Route path="/" exact={true}>
-      <Home />
-    </Route>
-    <Route path="/about">
-      <About />
-    </Route>
-    <Route path="/features">
-      <Features />
-    </Route>
-    </Switch>
+      <div className="app">
+        <div className="browser">
+          <div className="tabs">
+            <Tab>
+              {" "}
+              <NavLink to="/" exact={true} activeClassName="is-active">
+                Home
+              </NavLink>
+            </Tab>
+            <Tab>
+              {" "}
+              <NavLink to="/about" activeClassName="is-active">
+                About
+              </NavLink>
+            </Tab>
+            <Tab>
+              {" "}
+              <NavLink to="/features" activeClassName="is-active">
+                Features
+              </NavLink>
+            </Tab>
+          </div>
+          <div className="viewport">
+            <Switch>
+              <Route path="/" exact={true}>
+                <Home />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/features">
+                <Features />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
     </Router>
   );
 }
