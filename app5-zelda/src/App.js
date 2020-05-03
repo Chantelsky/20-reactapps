6102892b26bbe8691c78f4fc5157c6d306c925ac
Chantelsky <chantel.sky@outlook.com>
2020-05-03 21:51:20 +1000

    setting up canvas and moving a box

---
+++ app5-zelda/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const canvasRef = useRef(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  //sets height and width of canvas
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+  }, []);
+
+  //move box if x or y changes
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.clearRect(0, 0, window.innerHeight, window.innerWidth);
+    context.fillRect(x, y, 100, 100);
+  }, [x, y]);
+
+  return (
+    <div className="app">
+      <canvas ref={canvasRef} />
+
+      <div className="arrows">
+        <button onClick={() => setY((y) => y - 20)}>Up</button>
+        <button onClick={() => setX((x) => x - 20)}>Left</button>
+        <button onClick={() => setY((y) => y + 20)}>Down</button>
+        <button onClick={() => setX((x) => x + 20)}>Right</button>
+      </div>
+
+      <div className="images">
+        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
+        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
+        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
