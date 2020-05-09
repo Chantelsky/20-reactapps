ca8d4cdc52b4e25a404bf1b2e578e1b7eff96f59
Chantelsky <chantel.sky@outlook.com>
2020-05-09 23:45:38 +1000

    useCallback and useState to update categories

--- app7-trivia/src/App.js
+++ app7-trivia/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Question from "./components/Question";
 import CategorySelector from "./components/CategorySelector";
 import ResultModal from "./components/ResultModal";
@@ -7,20 +7,24 @@ import "./App.css";
 
 export default function App() {
   const [question, setQuestion] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState("any");
 
-  useEffect(() => {
-    getQuestion();
-  }, []);
+  const getQuestion = useCallback(() => {
+    let url = "https://opentdb.com/api.php?amount=1";
+    if (selectedCategory !== "any") url += `&category=${selectedCategory}`;
 
-  function getQuestion() {
-    const url = "https://opentdb.com/api.php?amount=1";
+    console.log(url);
 
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setQuestion(data.results[0]);
       });
-  }
+  }, []);
+
+  useEffect(() => {
+    getQuestion();
+  }, [getQuestion, selectedCategory]);
 
   return (
     <div className="app">
@@ -29,7 +33,10 @@ export default function App() {
 
       {/* question header ----------------------- */}
       <div className="question-header">
-        <CategorySelector />
+        <CategorySelector
+          category={selectedCategory}
+          chooseCategory={setSelectedCategory}
+        />
         <Scoreboard />
       </div>
 
